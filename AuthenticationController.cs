355cb47ec623845925ebb6ed8a7b7c771814ac19
Afrath49 <afrath4964@gmail.com>
2022-12-17 14:20:49 +0530

    m

--- AuthenticationController.cs
+++ AuthenticationController.cs
@@ -5,7 +5,7 @@ using System.Security.Cryptography;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+//more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace JWTAuthentication
 {
